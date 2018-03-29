bbb2a8fd0ed64dd745639491cd266f6f525be1a6
JS Morisset <jsm@surniaulula.com>
2018-03-29 05:27:05 -0600

    v1.0.0

--- lib/config.php
+++ lib/config.php
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoTieConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO Core',
 						'name' => 'WPSSO Core',
-						'min_version' => '3.56.3-dev.3',
+						'min_version' => '3.56.3',
 					),
 					'img' => array(
 						'icons' => array(
