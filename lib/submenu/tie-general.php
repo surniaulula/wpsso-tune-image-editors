57b3b95c907fedeaa97e6445cde0c73a08a1652a
JS Morisset <jsm@surniaulula.com>
2018-10-08 08:12:08 -0600

    v1.2.0

--- lib/submenu/tie-general.php
+++ lib/submenu/tie-general.php
@@ -23,10 +23,10 @@ if ( ! class_exists( 'WpssoTieSubmenuTieGeneral' ) && class_exists( 'WpssoAdmin'
 				$this->p->debug->mark();
 			}
 
-			$this->menu_id = $id;
+			$this->menu_id   = $id;
 			$this->menu_name = $name;
-			$this->menu_lib = $lib;
-			$this->menu_ext = $ext;
+			$this->menu_lib  = $lib;
+			$this->menu_ext  = $ext;
 		}
 
 		/**
@@ -49,11 +49,11 @@ if ( ! class_exists( 'WpssoTieSubmenuTieGeneral' ) && class_exists( 'WpssoAdmin'
 
 			$this->implementations = apply_filters( 'wp_image_editors', array( 'WP_Image_Editor_Imagick', 'WP_Image_Editor_GD' ) );
 
-			add_meta_box( $this->pagehook.'_wp',
+			add_meta_box( $this->pagehook . '_wp',
 				_x( 'WordPress Settings', 'metabox title', 'wpsso-tune-image-editors' ), 
 					array( $this, 'show_metabox_wp' ), $this->pagehook, 'normal' );
 
-			add_meta_box( $this->pagehook.'_ext',
+			add_meta_box( $this->pagehook . '_ext',
 				_x( 'PHP Extension Settings', 'metabox title', 'wpsso-tune-image-editors' ), 
 					array( $this, 'show_metabox_ext' ), $this->pagehook, 'normal' );
 		}
@@ -61,21 +61,26 @@ if ( ! class_exists( 'WpssoTieSubmenuTieGeneral' ) && class_exists( 'WpssoAdmin'
 		public function show_metabox_wp() {
 
 			$metabox_id = 'tie-wp';
-			$tab_key = 'general';
+			$tab_key    = 'general';
 
-			$this->p->util->do_metabox_table( apply_filters( SucomUtil::sanitize_hookname( $this->p->lca.'_'.$metabox_id.'_'.$tab_key.'_rows' ),
-				$this->get_table_rows( $metabox_id, $tab_key ), $this->form ), 'metabox-'.$metabox_id.'-'.$tab_key );
+			$this->p->util->do_metabox_table( apply_filters( SucomUtil::sanitize_hookname( $this->p->lca . '_' . $metabox_id . '_' . $tab_key . '_rows' ),
+				$this->get_table_rows( $metabox_id, $tab_key ), $this->form ), 'metabox-' . $metabox_id . '-' . $tab_key );
 		}
 
 		public function show_metabox_ext() {
 
 			$metabox_id = 'tie-ext';
-			$tabs = array( 'imagick' => _x( 'ImageMagick', 'metabox tab', 'wpsso-tune-image-editors' ) );
+			$tabs       = array( 'imagick' => _x( 'ImageMagick', 'metabox tab', 'wpsso-tune-image-editors' ) );
 			$table_rows = array();
 
 			foreach ( $tabs as $tab_key => $title ) {
-				$table_rows[$tab_key] = array_merge( $this->get_table_rows( $metabox_id, $tab_key ), 
-					apply_filters( SucomUtil::sanitize_hookname( $this->p->lca.'_'.$metabox_id.'_'.$tab_key.'_rows' ), array(), $this->form ) );
+
+				$filter_name = SucomUtil::sanitize_hookname( $this->p->lca . '_' . $metabox_id . '_' . $tab_key . '_rows' );
+
+				$table_rows[ $tab_key ] = array_merge(
+					$this->get_table_rows( $metabox_id, $tab_key ), 
+					(array) apply_filters( $filter_name, array(), $this->form )
+				);
 			}
 
 			$this->p->util->do_metabox_tabbed( $metabox_id, $tabs, $table_rows );
@@ -85,14 +90,14 @@ if ( ! class_exists( 'WpssoTieSubmenuTieGeneral' ) && class_exists( 'WpssoAdmin'
 
 			$table_rows = array();
 
-			switch ( $metabox_id.'-'.$tab_key ) {
+			switch ( $metabox_id . '-' . $tab_key ) {
 
 				case 'tie-wp-general':
 
-					$table_rows[] = ''.
+					$table_rows[] = '' . 
 					$this->form->get_th_html( _x( 'Default WordPress Image Editor(s)',
-						'option label', 'wpsso-tune-image-editors' ), '', 'tie_wp_image_editors' ).
-					'<td>'.$this->form->get_select( 'tie_wp_image_editors', $this->p->cf['form']['editors'], '', '', true ).'</td>';
+						'option label', 'wpsso-tune-image-editors' ), '', 'tie_wp_image_editors' ) . 
+					'<td>' . $this->form->get_select( 'tie_wp_image_editors', $this->p->cf['form']['editors'], '', '', true ) . '</td>';
 
 					break;
 
@@ -101,35 +106,35 @@ if ( ! class_exists( 'WpssoTieSubmenuTieGeneral' ) && class_exists( 'WpssoAdmin'
 					$wp_imagick_position = array_search( 'WP_Image_Editor_Imagick', $this->implementations );
 
 					if ( $wp_imagick_position !== false ) {
-						$wp_imagick_status = '<font color="green">'.sprintf( __( 'Used as editor #%d',
-							'wpsso-tune-image-editors' ), $wp_imagick_position + 1 ).'</font>';
+						$wp_imagick_status = '<font color="green">' . sprintf( __( 'Used as editor #%d',
+							'wpsso-tune-image-editors' ), $wp_imagick_position + 1 ) . '</font>';
 					} else {
-						$wp_imagick_status = '<font color="red">'.__( 'Not used', 'wpsso-tune-image-editors' ).'</font>';
+						$wp_imagick_status = '<font color="red">' . __( 'Not used', 'wpsso-tune-image-editors' ) . '</font>';
 					}
 
 					if ( extension_loaded( 'imagick' ) ) {
-						$php_imagick_status = '<font color="green">'.__( 'Loaded', 'wpsso-tune-image-editors' ).'</font>';
+						$php_imagick_status = '<font color="green">' . __( 'Loaded', 'wpsso-tune-image-editors' ) . '</font>';
 					} else {
-						$php_imagick_status = '<font color="red">'.__( 'Not loaded', 'wpsso-tune-image-editors' ).'</font>';
+						$php_imagick_status = '<font color="red">' . __( 'Not loaded', 'wpsso-tune-image-editors' ) . '</font>';
 					}
 
-					$table_rows[] = ''.
+					$table_rows[] = '' . 
 					$this->form->get_th_html( sprintf( _x( 'WordPress %s Editor',
-						'option label', 'wpsso-tune-image-editors' ), 'ImageMagick' ), '', 'tie_wp_imagick_avail' ).
-					'<td><strong>'.$wp_imagick_status.'</strong></td>';
+						'option label', 'wpsso-tune-image-editors' ), 'ImageMagick' ), '', 'tie_wp_imagick_avail' ) . 
+					'<td><strong>' . $wp_imagick_status . '</strong></td>';
 
-					$table_rows[] = ''.
+					$table_rows[] = '' . 
 					$this->form->get_th_html( sprintf( _x( 'PHP %s Extension',
-						'option label', 'wpsso-tune-image-editors' ), 'ImageMagick' ), '', 'tie_php_imagick_avail' ).
-					'<td><strong>'.$php_imagick_status.'</strong></td>';
+						'option label', 'wpsso-tune-image-editors' ), 'ImageMagick' ), '', 'tie_php_imagick_avail' ) . 
+					'<td><strong>' . $php_imagick_status . '</strong></td>';
 
-					$table_rows['subsection_imagick_jpeg'] = '<td colspan="2" class="subsection"><h4>'.
-						sprintf( _x( '%s Resized Images', 'metabox title', 'wpsso-tune-image-editors' ), 'JPEG' ).'</h4></td>';
+					$table_rows['subsection_imagick_jpeg'] = '<td colspan="2" class="subsection"><h4>' . 
+						sprintf( _x( '%s Resized Images', 'metabox title', 'wpsso-tune-image-editors' ), 'JPEG' ) . '</h4></td>';
 
-					$table_rows[] = ''.
+					$table_rows[] = '' . 
 					$this->form->get_th_html( sprintf( _x( 'Adjust %s Images',
-						'option label', 'wpsso-tune-image-editors' ), 'JPEG' ), '', 'tie_imagick_jpeg_adjust' ).
-					'<td>'.$this->form->get_checkbox( 'tie_imagick_jpeg_adjust' ).'</td>';
+						'option label', 'wpsso-tune-image-editors' ), 'JPEG' ), '', 'tie_imagick_jpeg_adjust' ) . 
+					'<td>' . $this->form->get_checkbox( 'tie_imagick_jpeg_adjust' ) . '</td>';
 
 					break;
 			}
