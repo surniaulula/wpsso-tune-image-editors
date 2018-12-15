f6ecbddfd9a3e95f53ed9d4905378b43ef11a1f4
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:49:19 -0600

    v1.2.1

--- lib/config-gettext-extract.php
+++
@@ -1,8 +0,0 @@
-<?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array
-_x( 'GD and ImageMagick', 'option value', 'wpsso-tune-image-editors' );
-_x( 'GD Only', 'option value', 'wpsso-tune-image-editors' );
-_x( 'ImageMagick and GD', 'option value', 'wpsso-tune-image-editors' );
-_x( 'ImageMagick Only', 'option value', 'wpsso-tune-image-editors' );
-_x( 'WP Image Editors', 'lib file description', 'wpsso-tune-image-editors' );
-_x( 'WPSSO Core add-on to provide tuning options for the WordPress image editors and PHP image extensions.', 'plugin description', 'wpsso-tune-image-editors' );
---
+++ lib/gettext-pro.php
@@ -0,0 +1,16 @@
+<?php die( 'These aren\'t the droids you\'re looking for...' );
+
+/**
+ * Extract of translatable text strings from the lib/pro classes.
+ */
+_x( 'Adjustment Filter Priority', 'option label', 'wpsso-tune-image-editors' );
+_x( 'Contrast Leveling', 'option label', 'wpsso-tune-image-editors' );
+_x( 'Compression Quality', 'option label', 'wpsso-tune-image-editors' );
+_x( 'Sharpening Sigma', 'option label', 'wpsso-tune-image-editors' );
+_x( 'recommended value is %1$s to %2$s', 'option comment', 'wpsso-tune-image-editors' );
+_x( 'Sharpening Radius', 'option label', 'wpsso-tune-image-editors' );
+_x( 'recommended value is 0 (auto)', 'option comment', 'wpsso-tune-image-editors' );
+_x( 'Sharpening Amount', 'option label', 'wpsso-tune-image-editors' );
+_x( 'recommended value is %1$s to %2$s', 'option comment', 'wpsso-tune-image-editors' );
+_x( 'Sharpening Threshold', 'option label', 'wpsso-tune-image-editors' );
+_x( 'recommended value is %1$s to %2$s', 'option comment', 'wpsso-tune-image-editors' );
