7067bcba3b5404a7a68fb9536afa7d5bf1556dc5
JS Morisset <jsm@surniaulula.com>
2019-06-20 07:07:25 -0700

    v2.0.0-dev.3

--- wpsso-tune-image-editors.php
+++ wpsso-tune-image-editors.php
@@ -13,7 +13,7 @@
  * Description: WPSSO Core add-on offers tuning options for the WordPress image editors and PHP image extensions.
  * Requires At Least: 3.8
  * Tested Up To: 5.2.1
- * Version: 2.0.0-dev.2
+ * Version: 2.0.0-dev.3
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
