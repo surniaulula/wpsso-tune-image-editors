326d9501ab68d35e3f61e1c4320429b625085207
JS Morisset <jsm@surniaulula.com>
2019-08-02 17:11:31 -0700

    v2.0.0

--- wpsso-tune-image-editors.php
+++ wpsso-tune-image-editors.php
@@ -10,7 +10,7 @@
  * Author URI: https://wpsso.com/
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
- * Description: WPSSO Core add-on to sharpen and improve WordPress thumbnails and resized images for social sharing and SEO.
+ * Description: Sharpen and improve WordPress thumbnails and resized images for social sharing and better SEO.
  * Requires At Least: 3.8
  * Tested Up To: 5.2.2
  * Version: 2.0.0
