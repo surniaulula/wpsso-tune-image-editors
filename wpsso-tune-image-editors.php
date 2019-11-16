6f6e42e2784d3c910d8e826518a4a469fc54a65d
JS Morisset <jsm@surniaulula.com>
2019-11-16 14:35:49 -0800

    v2.0.2

--- wpsso-tune-image-editors.php
+++ wpsso-tune-image-editors.php
@@ -26,7 +26,7 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	die( 'These aren\'t the droids you\'re looking for...' );
+	die( 'These aren\'t the droids you\'re looking for.' );
 }
 
 if ( ! class_exists( 'WpssoTie' ) ) {
@@ -53,9 +53,10 @@ if ( ! class_exists( 'WpssoTie' ) ) {
 
 		public function __construct() {
 
-			require_once ( dirname( __FILE__ ) . '/lib/config.php' );
+			require_once dirname( __FILE__ ) . '/lib/config.php';
 
 			WpssoTieConfig::set_constants( __FILE__ );
+
 			WpssoTieConfig::require_libs( __FILE__ );	// Includes the register.php class library.
 
 			$this->reg = new WpssoTieRegister();		// Activate, deactivate, uninstall hooks.
