c25cb5281e5d1eb51a157db765f4c0a30f57909f
JS Morisset <jsm@surniaulula.com>
2019-12-12 12:46:09 -0800

    v2.0.3

--- wpsso-tune-image-editors.php
+++ wpsso-tune-image-editors.php
@@ -13,7 +13,7 @@
  * Description: Sharpen and improve WordPress thumbnails and resized images for social sharing and better SEO.
  * Requires PHP: 5.5
  * Requires At Least: 3.9
- * Tested Up To: 5.3
+ * Tested Up To: 5.3.1
  * Version: 2.0.3
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
