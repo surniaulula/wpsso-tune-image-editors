185439474ea76e108075f031469fe68ada149352
JS Morisset <jsm@surniaulula.com>
2020-04-06 10:30:29 -0700

    v2.1.0

--- wpsso-tune-image-editors.php
+++ wpsso-tune-image-editors.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.6
  * Requires At Least: 4.2
  * Tested Up To: 5.4
- * Version: 2.1.0-rc.3
+ * Version: 2.1.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
@@ -184,6 +184,11 @@ if ( ! class_exists( 'WpssoTie' ) ) {
 			}
 
 			if ( ! $this->have_min_version ) {
+
+				if ( $this->p->debug->enabled ) {
+					$this->p->debug->log( 'exiting early: have_min_version is false' );
+				}
+
 				return;	// Stop here.
 			}
 
