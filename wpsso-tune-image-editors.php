0df50db1e18fe3abf86af811badf1982025b975f
JS Morisset <jsm@surniaulula.com>
2020-04-29 17:56:59 -0700

    v2.2.0

--- wpsso-tune-image-editors.php
+++ wpsso-tune-image-editors.php
@@ -13,7 +13,7 @@
  * Description: Improves the Look of Thumbnails and Resized Images for Better Click-Through-Rates on Social and Search Sites.
  * Requires PHP: 5.6
  * Requires At Least: 4.2
- * Tested Up To: 5.4
+ * Tested Up To: 5.4.1
  * Version: 2.2.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
