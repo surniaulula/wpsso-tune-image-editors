fd54b20727b6d546c5f532363ba466f22fb71658
JS Morisset <jsm@surniaulula.com>
2020-09-13 11:05:33 -0700

    v2.5.0

---
+++ gettext/gettext-config.php
@@ -0,0 +1,11 @@
+<?php die( 'These aren\'t the droids you\'re looking for.' );
+
+/**
+ * Extract of translatable strings from the config array.
+ */
+_x( 'Image Editors', 'lib file description', 'wpsso-tune-image-editors' );
+_x( 'Improves the Look of Thumbnails and Resized Images for Better Click-Through-Rates on Social and Search Sites.', 'plugin description', 'wpsso-tune-image-editors' );
+_x( 'GD Only', 'option value', 'wpsso-tune-image-editors' );
+_x( 'GD and ImageMagick', 'option value', 'wpsso-tune-image-editors' );
+_x( 'ImageMagick Only', 'option value', 'wpsso-tune-image-editors' );
+_x( 'ImageMagick and GD', 'option value', 'wpsso-tune-image-editors' );
