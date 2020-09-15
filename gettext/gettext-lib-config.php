a0dddcec4465035f1445d1cfb538709d458c0657
JS Morisset <jsm@surniaulula.com>
2020-09-15 09:21:53 -0700

    v2.5.0

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -4,6 +4,7 @@
  * Extract of translatable strings from lib/config.php.
  */
 _x( 'Image Editors', 'lib file description', 'wpsso-tune-image-editors' );
+_x( 'WPSSO Tune Image Editors', 'plugin name', 'wpsso-tune-image-editors' );
 _x( 'Improves the Look of Thumbnails and Resized Images for Better Click-Through-Rates on Social and Search Sites.', 'plugin description', 'wpsso-tune-image-editors' );
 _x( 'GD Only', 'option value', 'wpsso-tune-image-editors' );
 _x( 'GD and ImageMagick', 'option value', 'wpsso-tune-image-editors' );
