d9cea4289f56cbdfe33a39a1eea2c20676ceddd1
JS Morisset <jsm@surniaulula.com>
2021-09-22 07:53:52 -0700

    v2.7.2

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -5,7 +5,7 @@
  */
 _x( 'Image Editors', 'lib file description', 'wpsso-tune-image-editors' );
 _x( 'WPSSO Tune WP Image Editors', 'plugin name', 'wpsso-tune-image-editors' );
-_x( 'Improves the appearance of WordPress images for better click-through-rates from social and search sites.', 'plugin description', 'wpsso-tune-image-editors' );
+_x( 'Improves the appearance of WordPress images for better click through rates from social and search sites.', 'plugin description', 'wpsso-tune-image-editors' );
 _x( 'GD Only', 'option value', 'wpsso-tune-image-editors' );
 _x( 'GD and ImageMagick', 'option value', 'wpsso-tune-image-editors' );
 _x( 'ImageMagick Only', 'option value', 'wpsso-tune-image-editors' );
--- wpsso-tune-image-editors.php
+++ wpsso-tune-image-editors.php
@@ -10,7 +10,7 @@
  * Author URI: https://wpsso.com/
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
- * Description: Improves the appearance of WordPress images for better click-through-rates from social and search sites.
+ * Description: Improves the appearance of WordPress images for better click through rates from social and search sites.
  * Requires PHP: 7.0
  * Requires At Least: 5.0
  * Tested Up To: 5.8.1
