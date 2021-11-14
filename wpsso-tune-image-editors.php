7a7e0d3125f3260c68073deff385bcf5bd76b60c
JS Morisset <jsm@surniaulula.com>
2021-11-13 16:39:44 -0800

    v2.8.1

--- wpsso-tune-image-editors.php
+++ wpsso-tune-image-editors.php
@@ -66,10 +66,7 @@ if ( ! class_exists( 'WpssoTie' ) ) {
 			load_plugin_textdomain( 'wpsso-tune-image-editors', false, 'wpsso-tune-image-editors/languages/' );
 		}
 
-		/**
-		 * $is_admin, $doing_ajax, and $doing_cron available since WPSSO Core v8.8.0.
-		 */
-		public function init_objects( $is_admin = false, $doing_ajax = false, $doing_cron = false ) {
+		public function init_objects() {
 
 			$this->p =& Wpsso::get_instance();
 
