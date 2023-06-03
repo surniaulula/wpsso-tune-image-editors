1438746c39c25bd92d8abfc032daa77f37ee6035
JS Morisset <jsm@surniaulula.com>
2023-06-03 10:29:36 -0700

    v3.0.0-dev.9

--- wpsso-tune-image-editors.php
+++ wpsso-tune-image-editors.php
@@ -15,7 +15,7 @@
  * Requires PHP: 7.2.5
  * Requires At Least: 5.5
  * Tested Up To: 6.2.2
- * Version: 3.0.0-dev.8
+ * Version: 3.0.0-dev.9
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
