456b606e167459f15e4ecaf69574cb10c6c13999
JS Morisset <jsm@surniaulula.com>
2023-11-08 07:15:40 -0800

    v4.0.0-rc.2

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoTieConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssotie' => array(			// Plugin acronym.
-					'version'     => '4.0.0-rc.1',	// Plugin version.
+					'version'     => '4.0.0-rc.2',	// Plugin version.
 					'opt_version' => '9',		// Increment when changing default option values.
 					'short'       => 'WPSSO TIE',	// Short plugin name.
 					'name'        => 'WPSSO Tune WP Image Editors',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoTieConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '17.0.0-rc.1',
+							'min_version'   => '17.0.0-rc.2',
 						),
 					),
 
