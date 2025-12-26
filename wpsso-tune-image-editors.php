754b0acd10e9e3b9e868113faa7337ecb72d6244
jsmoriss <jsm@surniaulula.com>
2025-12-26 16:42:50 +0000

    v4.1.0

--- wpsso-tune-image-editors.php
+++ wpsso-tune-image-editors.php
@@ -13,7 +13,7 @@
  * Description: Improves the appearance of WordPress images for better click through rates from social and search sites.
  * Requires Plugins: wpsso
  * Requires PHP: 7.4.33
- * Requires At Least: 5.9
+ * Requires At Least: 6.0
  * Tested Up To: 6.9
  * Version: 4.1.0
  *
