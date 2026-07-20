f6df3eb53e8b29165850504d15d2a4b2d0ecf574
jsmoriss <jsm@surniaulula.com>
2026-07-20 14:20:30 +0000

    v4.1.0

--- lib/register.php
+++ lib/register.php
@@ -81,7 +81,7 @@ if ( ! class_exists( 'WpssoTieRegister' ) ) {
 
 				global $wpdb;
 
-				$db_query = 'SELECT blog_id FROM '.$wpdb->blogs;
+				$db_query = 'SELECT blog_id FROM ' . $wpdb->blogs;
 
 				$blog_ids = $wpdb->get_col( $db_query );
 
